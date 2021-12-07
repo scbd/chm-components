ebe197459c3ba16ad4be82705654245150fb82af
Randy Houlahan <randy.houlahan@cbd.int>
2021-12-07 12:17:01 -0500

    vite init with purgecss

---
+++ packages/submit-list/src/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <submit-list></submit-list>
+</template>
+
+<script>
+import SubmitList from './components/SubmitList.vue';
+
+export default {
+  name      : 'App',
+  components: {
+    SubmitList,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+@import "./assets/css/styles.css";
+</style>
--- packages/submit-list/src/main.js
+++ packages/submit-list/src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue';
 import { createI18n } from 'vue-i18n';
-import App from './App.vue';
+import App from './index.vue';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
+import './assets/css/styles.css';
+
+
 
 const i18n = createI18n<false>({
   legacy  : false,
