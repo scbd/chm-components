0b5bacb478e94adabfed64766e836421984e58f6
Hitesh Patiar <james.barnard.consultant@cbd.int>
2022-02-16 23:44:55 +0530

    setup search vite

---
+++ packages/search-vite/src/Icons/Icon.vue
@@ -0,0 +1,13 @@
+<template >
+  <svg :class="{ [`aa-icon-${name}`]:true }" class="aa-icon" >
+    <use :xlink:href="`#aa-icon-${name}`"></use>
+  </svg>
+</template>
+<script>
+export default {
+  name : 'AAIcon',
+  // #region snippet
+  props: [ 'name' ],
+// #endregion snippet
+};
+</script>
