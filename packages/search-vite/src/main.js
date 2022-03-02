0b5945f324968fad2e779fcc5c398cbfa0da4795
Hitesh Patiar <james.barnard.consultant@cbd.int>
2022-03-02 18:59:38 +0530

    fix vue-i18n issue

--- packages/search-vite/src/main.js
+++ packages/search-vite/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createI18n } from 'vue-i18n';
+import { createI18n } from 'vue-i18n/dist/vue-i18n.esm-browser.prod.js';
+// import { createI18n } from '../../node_modules/vue-i18n/dist/vue-i18n';
 import messages from './locales/index'
 const i18n = createI18n(false, { legacy: false, locale: 'en', messages, global: true });
 
